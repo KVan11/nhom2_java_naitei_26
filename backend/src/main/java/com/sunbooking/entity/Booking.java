604d67e3add98e66bc5ce3d887ee0d2184239d8c
KVan11 <nguyenvan2005ty@gmail.com>
2026-08-20 22:09:19 +0700

    feat: tích hợp Swagger

--- backend/src/main/java/com/sunbooking/entity/Booking.java
+++ backend/src/main/java/com/sunbooking/entity/Booking.java
@@ -1,4 +1,13 @@
 package com.sunbooking.entity;
 
-public class Booking {
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Table(name = "bookings")
+@Getter
+@Setter
+public class Booking extends BaseEntity {
 }
